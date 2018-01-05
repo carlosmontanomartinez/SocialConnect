bcd7402a97fe7fe457c8ab3061515c228cb9ce36
cmontano <cmontano@IADW-274>
2018-01-05 16:12:13 -0600

    Social connect performed

---
+++ SocialConnect/wwwroot/js/facebook.connect.pkg.js
@@ -0,0 +1,22 @@
+﻿window.fbAsyncInit = function () {
+    var x = FB.init({
+        appId: 772904349527252,
+        status: true,
+        cookie: true,
+        oauth: true,
+        xfbml: true,
+        version: 'v2.5'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.async = true;
+    js.src = '//connect.facebook.net/es_LA/sdk.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
